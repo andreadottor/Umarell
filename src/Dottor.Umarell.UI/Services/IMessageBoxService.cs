049fb3bb8f5c569a89ab513b3479f69b071624a7
Andrea Dottor <andrea@dottor.net>
2022-02-18 20:29:05 +0100

    clean code

---
+++ src/Dottor.Umarell.UI/Services/IMessageBoxService.cs
@@ -0,0 +1,11 @@
+﻿namespace Dottor.Umarell.UI.Services
+{
+    public record MessageBoxShowEventArgs(string Title, string Text, MessageBoxType MessageBoxType, Func<bool, Task> ReturnCallback);
+
+    public interface IMessageBoxService
+    {
+        event EventHandler<MessageBoxShowEventArgs>? MessageBoxShow;
+        Task ShowAlertAsync(string title, string text);
+        Task<bool> ShowConfirmAsync(string title, string text);
+    }
+}
--- src/Dottor.Umarell.UI/Services/MessageBoxService.cs
+++ src/Dottor.Umarell.UI/Services/MessageBoxService.cs
@@ -3,14 +3,6 @@
     using System;
     using System.Threading.Tasks;
 
-    public record MessageBoxShowEventArgs(string Title, string Text, MessageBoxType MessageBoxType, Func<bool, Task> ReturnCallback);
-
-    public interface IMessageBoxService
-    {
-        event EventHandler<MessageBoxShowEventArgs>? MessageBoxShow;
-        Task ShowAlertAsync(string title, string text);
-        Task<bool> ShowConfirmAsync(string title, string text);
-    }
 
     public class MessageBoxService : IMessageBoxService
     {
