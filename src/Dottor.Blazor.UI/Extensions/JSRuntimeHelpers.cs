cf9cca166c8de94a7c750e2a65a6f4ebb72b6624
Andrea Dottor <andrea@dottor.net>
2022-05-18 10:08:30 +0200

    Extension method for import js module

---
+++ src/Dottor.Blazor.UI/Extensions/JSRuntimeHelpers.cs
@@ -0,0 +1,17 @@
+﻿namespace Dottor.Blazor.UI.Extensions;
+
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+public static class JSRuntimeHelpers
+{
+    public static async ValueTask<IJSObjectReference> ImportModuleAsync(this IJSRuntime jsRuntime, string scriptPath)
+    {
+        return await jsRuntime.InvokeAsync<IJSObjectReference>("import", $"{scriptPath}?v={VersionUtility.GetVersion()}");
+    }
+
+    public static async ValueTask<IJSInProcessObjectReference> ImportModuleAsync(this IJSInProcessRuntime jsInProcessRuntime, string scriptPath)
+    {
+        return await jsInProcessRuntime.InvokeAsync<IJSInProcessObjectReference>("import", $"{scriptPath}?v={VersionUtility.GetVersion()}");
+    }
+}
---
+++ src/Dottor.Blazor.UI/VersionUtility.cs
@@ -0,0 +1,31 @@
+﻿namespace Dottor.Blazor.UI;
+
+using System.Reflection;
+
+public static class VersionUtility
+{
+    private static string? _version;
+
+#if DEBUG
+    public static string GetVersion()
+    {
+        if (string.IsNullOrWhiteSpace(_version))
+        {
+            _version = Guid.NewGuid().ToString();
+        }
+        return _version;
+    }
+#else
+    public static string GetVersion()
+    {
+        if (string.IsNullOrWhiteSpace(_version))
+        {
+            _version = Assembly.GetExecutingAssembly()
+                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                        .InformationalVersion;
+        }
+        return _version;
+    }
+#endif
+
+}
