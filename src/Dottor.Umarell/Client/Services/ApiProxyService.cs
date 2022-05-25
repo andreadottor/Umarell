e6402743bb351ff8f36c0e4c3457b2ac75444e69
Andrea Dottor <andrea@dottor.net>
2022-05-25 21:09:59 +0200

    format code

--- src/Dottor.Umarell/Client/Services/ApiProxyService.cs
+++ src/Dottor.Umarell/Client/Services/ApiProxyService.cs
@@ -1,41 +1,41 @@
-﻿namespace Dottor.Umarell.Client.Services
+﻿namespace Dottor.Umarell.Client.Services;
+
+using Dottor.Umarell.Shared;
+using System.Net.Http.Json;
+
+public class ApiProxyService
 {
-    using Dottor.Umarell.Shared;
-    using System.Net.Http.Json;
+    private readonly HttpClient _httpClient;
 
-    public class ApiProxyService
+    public ApiProxyService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
-
-        public ApiProxyService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-
-        public async ValueTask<bool> InsertBuildingSiteAsync(BuildingSiteInsertModel model)
-        {
-            var response = await _httpClient.PostAsJsonAsync($"api/v1/BuildingSites", model);
-            if (response.IsSuccessStatusCode)
-            {
-                var data = await response.Content.ReadFromJsonAsync<ApiResult>();
-                return data.Result;
-            }
-
-            return false;
-        }
-
-        public async ValueTask<IEnumerable<BuildingSiteModel>> GetAllBuildingSiteAsync()
-        {
-            var response = await _httpClient.GetAsync($"api/v1/BuildingSites");
-            if (response.IsSuccessStatusCode)
-            {
-                var data = await response.Content.ReadFromJsonAsync<ApiResult<IEnumerable<BuildingSiteModel>>>();
-                if (data.Result)
-                    return data.Data;
-            }
-
-            throw new Exception("Error on retrieve list of BuildingSites");
-        }
-
+        _httpClient = httpClient;
     }
+
+    public async ValueTask<bool> InsertBuildingSiteAsync(BuildingSiteInsertModel model)
+    {
+        var response = await _httpClient.PostAsJsonAsync($"api/v1/BuildingSites", model);
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadFromJsonAsync<ApiResult>();
+            if(data is not null)
+                return data.Result; 
+        }
+
+        return false;
+    }
+
+    public async ValueTask<IEnumerable<BuildingSiteModel>> GetAllBuildingSiteAsync()
+    {
+        var response = await _httpClient.GetAsync($"api/v1/BuildingSites");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadFromJsonAsync<ApiResult<IEnumerable<BuildingSiteModel>>>();
+            if (data is not null && data.Result)
+                return data.Data;
+        }
+
+        throw new Exception("Error on retrieve list of BuildingSites");
+    }
+
 }
